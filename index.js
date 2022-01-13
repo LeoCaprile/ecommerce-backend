569a66a4ec45cf701f97b5b8958504510b9c05ed
Matías Fuentes <matiasf.cortez@gmail.com>
2022-01-12 23:24:20 -0300

    added 404 page not found

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ app.use('/api/getProducts', getProducts);
 app.use('/api/getCategories', getCategories);
 app.use('/api/getDiscounts', getDiscounts);
 
+app.all('*', (req, res) => {
+	res.status(404).send('<h1>404, Ruta no encontrada!</h1><h2>Intenta"/api/getProducts"</h2>');
+});
+
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
